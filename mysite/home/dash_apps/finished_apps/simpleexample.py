dab0be178af3aa5deb54f3bb9a95b9d0bba1d1d7
Ryan Porter <58050063+RyPort@users.noreply.github.com>
2021-03-18 16:36:14 -0400

    add custom input graph

--- mysite/home/dash_apps/finished_apps/simpleexample.py
+++ mysite/home/dash_apps/finished_apps/simpleexample.py
@@ -1,6 +1,6 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 from django_plotly_dash import DjangoDash
 from jikanpy import Jikan
@@ -11,6 +11,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = DjangoDash('SimpleExample', external_stylesheets=external_stylesheets)
 app2 = DjangoDash('Graph2', external_stylesheets=external_stylesheets)
+app3 = DjangoDash('inputgraph', external_stylesheets=external_stylesheets)
 
 def get_topanimenames(rank:int, subtype:str):
     aio_jikan_2 = Jikan()
@@ -118,6 +119,63 @@ def display_value2(value2):
             a=kw_grplist[i][0].split()[:4]
             b = [' '.join(kw_grplist[i][0].split()[:4])]
             trendshow.build_payload(b,timeframe = 'today 12-m',geo='')
+            temp = trendshow.interest_over_time()
+
+        dic[i] = temp
+
+
+
+    trendframe = pd.concat(dic,axis=1)
+    trendframe.columns = trendframe.columns.droplevel(0)
+    trendframe = trendframe.drop('isPartial',axis=1)
+    
+    
+    trace = [go.Scatter(
+        x = trendframe.index,
+        y=trendframe[col], name = col) for col in trendframe.columns]
+    layout = dict(
+        paper_bgcolor='#27293d',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(color='white'),
+    )
+    return {'data': trace, 'layout': layout}
+
+app3.layout = html.Div([
+    html.H1('Custom Input Graph'),
+    html.Button("Custom export", id="export_table", **{"data-dummy": ""}),
+    dcc.Input(id='input1', type='text', value='Sailor Moon'),
+    html.Button(id='submit-button', n_clicks=0, children='Submit'),
+    dcc.Graph(id='slider-graph3', animate=True, style={"backgroundColor": "#1a2d46", 'color': '#ffffff'}),
+    dcc.Slider(
+        id='slider-updatemode2',
+        marks={i: '{}'.format(i) for i in range(20)},
+        max=20,
+        value=2,
+        step=1,
+        updatemode='drag',
+        min=0,
+    ),
+])
+@app3.callback(
+               Output('slider-graph3', 'figure'),
+              [Input('submit-button','n_clicks'),
+              State('input1','value')]
+)
+def display_value3(n_clicks, value):
+
+    trendshow = TrendReq(hl='en-US', tz=360)
+
+    kw_list = [value]
+    kw_group = list(zip(*[iter(kw_list)]*1))
+    kw_grplist = [list(x) for x in kw_group]
+    dic = {}
+    for i in range(0,len(kw_grplist)):
+        trendshow.build_payload(kw_grplist[i],timeframe = 'today 5-y',geo='')
+        temp = trendshow.interest_over_time()
+        if temp.empty:
+            a=kw_grplist[i][0].split()[:4]
+            b = [' '.join(kw_grplist[i][0].split()[:4])]
+            trendshow.build_payload(b,timeframe = 'today 5-y',geo='')
             temp = trendshow.interest_over_time()
 
         dic[i] = temp
