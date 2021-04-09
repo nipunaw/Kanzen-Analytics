fd6bd440c5fe5c2ef68d53cd64b18618fb08f7a6
Nipuna Weerapperuma <nipunaw12@gmail.com>
2021-04-09 02:44:24 -0400

    Database integrated with export, edit pages. Refactored code.
    Note that export page performs same functionality as edit page.

--- mysite/home/dash_apps/finished_apps/edit_container.py
+++ mysite/home/dash_apps/finished_apps/edit_container.py
@@ -1,27 +1,21 @@
-import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 from dash.dependencies import Input, Output, State
-import plotly.graph_objs as go
 from django_plotly_dash import DjangoDash
-from jikanpy import Jikan
-from pytrends.request import TrendReq
-import pandas as pd
-from home.dash_apps.finished_apps import graphs
+from home.models import Anime
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 class Edit_Container:
-    def __init__(self,id:str):
-        self.graphs_list = ["Demon Slayer","Sailor Moon", "Jojo"]
-        #print(self.graphs_list)
+
+    def __init__(self, id:str, shared_info):
+        self.shared_info = shared_info
+        self.shared_info.pending_updates_edit = True
+        self.graphs_list = []
         self.app = DjangoDash(id, external_stylesheets=external_stylesheets)
-        self.app.layout = html.Div([
-            html.H1("Edit Page"),
-            self.init_table('Remove', 5),
-            html.Button('Remove Selected Graphs',id="remove_graphs",n_clicks=0),
-            html.Div(id="graphcontainer")
-        ])
+        self.table = dash_table.DataTable()
+        self.app.layout = self.serve_layout
+
         @self.app.callback(
             [Output('table', 'data'),
             Output('table', 'dropdown')],
@@ -37,35 +31,53 @@ class Edit_Container:
                         ]
                     }
                 }
-            
+
 
             graphs_to_remove = []
-            #print(self.graphs_list)
-            if n_clicks >0:
-                #print(data)
+            if n_clicks > 0:
+                self.shared_info.pending_updates_main = True
+                self.shared_info.pending_updates_export = True
                 for i in value:
-                    #print(i)
                     if i['Remove Graph(s)'] == 'Remove Graph':
-                        graphs_to_remove.append(i['Name'])    
+                        graphs_to_remove.append(i['Name'])
                 for name in graphs_to_remove:
                         self.graphs_list.remove(name)
-                        #print(self.graphs_list)
-                        
+                        Anime.objects.filter(pk=name).delete()
+
             for title in self.graphs_list:
                     data.append({'Name':title,'Remove Graph(s)':"Don't remove Graph"})
             return data, dropdown
+
+    def serve_layout(self):
+        if self.shared_info.pending_updates_edit:
+            self.shared_info.pending_updates_edit = False
+            self.table = self.init_table('Remove', 5)
+
+        return html.Div([
+                html.H1("Edit Page"),
+                self.table,
+                html.Button('Remove Selected Graphs', id="remove_graphs", n_clicks=0)
+        ])
+
+
     def init_table(self, type:str='Add', pg_size:int=5):
         layout = dash_table.DataTable(
             id='table',
             columns=[{"name": "Name", "id": "Name"}, {"name": type+' Graph(s)', "id": type+' Graph(s)', "presentation": "dropdown"}],
-            data=[{'Name': "", type+' Graph(s)': ""},
-                  {'Name': "", type+' Graph(s)': ""},
-                  {'Name': "", type+' Graph(s)': ""},
-                  {'Name': "", type+' Graph(s)': ""},
-                  {'Name': "", type+' Graph(s)': ""}],
+            data=self.init_data(),
             page_size=pg_size,
             editable=True,
             #row_deletable=True,
         )
 
         return layout
+
+    def init_data(self):
+        data = []
+        self.graphs_list = []
+        for a in Anime.objects.raw('SELECT * FROM home_anime'):
+            p = str(a)
+            self.graphs_list.append(p)
+        for title in self.graphs_list:
+            data.append({'Name': title, 'Remove Graph(s)': "Don't remove Graph"})
+        return data
--- mysite/home/dash_apps/finished_apps/init.py
+++ mysite/home/dash_apps/finished_apps/init.py
@@ -1,97 +1,15 @@
-from home.dash_apps.finished_apps import graphs
 from home.dash_apps.finished_apps import container
 from home.dash_apps.finished_apps import edit_container
 from home.dash_apps.finished_apps import export_container
-from dash.dependencies import Input, Output, State
-from home.models import Anime
-import time
+from home.dash_apps.finished_apps import shared_info
 
-initial_graphs = []
+
+# initial_graphs = []
 #top_shows = graphs.Graphs('top5anime', 'Top 5 Anime', 'topanime', 'topanimeslider', True, [Input('topanimeslider', 'value')])
-
 #initial_graphs.append(top_shows)
 
-for a in Anime.objects.raw('SELECT * FROM home_anime'):
-     p = str(a)
-     time.sleep(2)
-     initial_graphs.append(graphs.Graphs(p.replace(" ", ""), p, p.replace(" ", ""), p.replace(" ", "")+'slider', False, [Input(p.replace(" ", "")+'slider', 'value')], p))
+shared_data = shared_info.Shared_Info()
 
-
-container_object = container.Container('container',initial_graphs)
-
-edit_page_container = edit_container.Edit_Container('edit_container')
-
-export_page_container = export_container.Export_Container('export_container')
-# import dash_core_components as dcc
-# import dash_html_components as html
-# from dash.dependencies import Input, Output
-# import plotly.graph_objs as go
-# from django_plotly_dash import DjangoDash
-# from jikanpy import Jikan
-#
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#
-# app = DjangoDash('SimpleExample', external_stylesheets=external_stylesheets)
-#
-#
-# def get_names():
-#     aio_jikan_2 = Jikan()
-#     top_anime = aio_jikan_2.top(type='anime', page=1, subtype='tv')
-#     return top_anime["top"][0]["title"]
-#
-#
-# app.layout = html.Div([
-#     html.H1(get_names()),
-#     html.Button("Custom export", id="export_table", **{"data-dummy": ""}),
-#     dcc.Graph(id='slider-graph', animate=True, style={"backgroundColor": "#1a2d46", 'color': '#ffffff'}),
-#     dcc.Slider(
-#         id='slider-updatemode',
-#         marks={i: '{}'.format(i) for i in range(20)},
-#         max=20,
-#         value=2,
-#         step=1,
-#         updatemode='drag',
-#         min=0,
-#     ),
-# ])
-#
-# app.clientside_callback(
-#     """
-#     function(n_clicks) {
-#         if (n_clicks > 0)
-#             document.querySelector("#slider-graph a.modebar-btn").click()
-#         return ""
-#     }
-#     """,
-#     Output("export_table", "data-dummy"),
-#     [Input("export_table", "n_clicks")]
-# )
-#
-# @app.callback(
-#                Output('slider-graph', 'figure'),
-#               [Input('slider-updatemode', 'value')])
-# def display_value(value):
-#
-#
-#     x = []
-#     for i in range(value):
-#         x.append(i)
-#
-#     y = []
-#     for i in range(value):
-#         y.append(i*i)
-#
-#     graph = go.Scatter(
-#         x=x,
-#         y=y,
-#         name='Manipulate Graph'
-#     )
-#     layout = go.Layout(
-#         paper_bgcolor='#27293d',
-#         plot_bgcolor='rgba(0,0,0,0)',
-#         xaxis=dict(range=[min(x), max(x)]),
-#         yaxis=dict(range=[min(y), max(y)]),
-#         font=dict(color='white'),
-#
-#     )
-#     return {'data': [graph], 'layout': layout}
+container_object = container.Container('container', shared_data)
+edit_page_container = edit_container.Edit_Container('edit_container', shared_data)
+export_page_container = export_container.Export_Container('export_container', shared_data)
