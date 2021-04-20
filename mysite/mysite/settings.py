5f706e542f2874cb44422458b4647622deff46ca
Nipuna Weerapperuma <nipunaw12@gmail.com>
2021-04-20 04:55:58 -0400

    Added static root

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -186,7 +186,7 @@ PLOTLY_COMPONENTS = [
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_LOCATION = 'static'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mysite/static')
 ]
